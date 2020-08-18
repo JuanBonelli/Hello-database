846b5768970e8e6c9e4446bf0e5ec6db0a738b88
JuanBonelli <67331365+JuanBonelli@users.noreply.github.com>
2020-08-18 17:14:22 -0300

    Update user.js

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,7 +1,16 @@
-const express = require('express');
-const router = express.Router();
-const User = require ('../../models/User');
+// user.js
 
+// usamos express
+const express = require('express');
+
+// creamos un router
+const router = express.Router();
+
+// importamos el modelo User
+// la ruta del archivo del modelo es relativa a la de user.js
+const User = require('../../models/User');
+
+// GET a /api/users (todos los usuarios)
 router.get('/users', (req, res) => {
   User.find((err, users) => {
     if (err) throw err;
@@ -9,6 +18,7 @@ router.get('/users', (req, res) => {
   });
 });
 
+// GET a /api/user/id (un solo usuario)
 router.get('/user/:id', (req, res) => {
   User.findOne({ id: req.params.id }, (err, user) => {
     if (err) throw err;
@@ -16,4 +26,5 @@ router.get('/user/:id', (req, res) => {
   });
 });
 
+// hay que exportar el router para usarlo en index.js
 module.exports = router;
